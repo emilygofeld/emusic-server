5d91d0e93c72f463dbb78ca824685c0a8633d4b3
emilygofeld <emiloren17@gmail.com>
2024-11-26 21:15:15 +0200

    added codes and sealed classes for music responses and requests

---
+++ src/main/kotlin/com/emily/core/ProtocolCode.kt
@@ -0,0 +1,19 @@
+package com.emily.core
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object ProtocolCode {
+    const val CREATE_PLAYLIST = 100
+    const val DELETE_PLAYLIST = 110
+    const val ADD_SONG_TO_PLAYLIST = 120
+    const val DELETE_SONG_FROM_PLAYLIST = 130
+    const val GET_PLAYLIST = 140
+    const val GET_USER_PLAYLISTS = 150
+    const val GET_CURR_USER_PLAYLISTS = 160
+    const val GET_SONG = 200
+    const val GET_USER_DATA = 300
+    const val GET_CURR_USER_DATA = 310
+    const val SUCCESS = 400
+    const val ERROR = 500
+}
